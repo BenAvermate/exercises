d2f2ba974f96e61460870f1c77eeb18e4b969a7a
Ben Avermate <ben.avermate@student.ucll.be>
2021-02-09 15:27:03 +0100

    finished

--- 01-basic-python/03-loops/01-is-prime/student.py
+++ 01-basic-python/03-loops/01-is-prime/student.py
@@ -1,1 +1,11 @@
-# Write your code here
+import math
+
+def is_prime(n):
+    if n == 0 or n == 1:
+        return False
+    x = math.floor(math.sqrt(n))
+    for i in range(2,x+1):
+        if (n % i) == 0:
+            return False
+    return True
+    #return n>1 zou beter zijn
--- 01-basic-python/03-loops/02-coins/student.py
+++ 01-basic-python/03-loops/02-coins/student.py
@@ -1,1 +1,8 @@
-# Write your code here
+def coins(c1, c2, c5, goal):
+    count = 0
+    for x in range(0, c1+1):
+        for y in range(0, c2+1):
+            for z in range(0, c5+1):
+                if (z * 5 + y * 2 + x) == goal:
+                    return True
+    return False
