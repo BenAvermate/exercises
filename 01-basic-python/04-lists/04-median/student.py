f59841c5bcac8a1219ba2113dd6f57fc6c7b56bf
Ben Avermate <ben.avermate@student.ucll.be>
2021-02-09 16:36:46 +0100

    finished

--- 01-basic-python/04-lists/04-median/student.py
+++ 01-basic-python/04-lists/04-median/student.py
@@ -1,1 +1,7 @@
-# Write your code here
+import math
+def median(ns):
+    ns = sorted(ns)
+    if len(ns)%2==1:
+        return ns[int(math.floor(len(ns)/2))]
+    else:
+        return (ns[int(len(ns)/2)]+ns[int(len(ns)/2-1)])/2
