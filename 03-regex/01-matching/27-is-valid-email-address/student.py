eaf7e2b8fc502d6f0682c225b37399551b7e64fe
Ben Avermate <ben.avermate@student.ucll.be>
2021-03-09 15:21:17 +0100

    finished

--- 03-regex/01-matching/27-is-valid-email-address/student.py
+++ 03-regex/01-matching/27-is-valid-email-address/student.py
@@ -1,1 +1,4 @@
-# Write your code here
+import re
+
+def is_valid_email_address(string):
+    return re.fullmatch(r'[0-9a-z\.]+@(student\.)?ucll\.be',string)
--- 03-regex/01-matching/28-is-valid-password/student.py
+++ 03-regex/01-matching/28-is-valid-password/student.py
@@ -1,1 +1,15 @@
-# Write your code here
+import re
+
+def is_valid_password(string):
+    mandatory = [
+        r'.{8,}',
+        r'[0-9]',
+        r'[a-z]',
+        r'[A-Z]',
+        r'[\+\-*/.@]',
+    ]
+    prohibited = [
+        r'(.)\1{2}',
+        r'(.)(.*\1){3}',
+    ]
+    return all(re.search(regex, string) for regex in mandatory) and not any(re.search(regex, string) for regex in prohibited)
